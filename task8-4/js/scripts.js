dd43307f3c02b49d33fd908e6d0187421c00f290
krystianan <krystian1906@gmail.com>
2017-02-02 19:22:21 +0100

    8-4

---
+++ task8-4/js/scripts.js
@@ -0,0 +1,4 @@
+var a = prompt("Wpisz podstawe");
+var h = prompt("Wpisz wysokosc");
+var traingleArea = a*h/2;
+console.log("Pole trojkata o podstawie a: " + a + " oraz wysokosci h: " + h + " wynosi: " + traingleArea)
